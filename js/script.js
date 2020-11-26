1dfaf42dfe42b964fa47065e9ce89556326a40ce
Nekushonok <Nekushonok@gmail.com>
2020-11-26 14:32:17 +0900

    Change leaving pictures in the background

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 let devicesAnimation = document.querySelectorAll(".devices-animation");
 let replayAnimationBtn = document.querySelector(".play-animation");
 
-function onReplayAnimBtn() {
+function onReplayAnimBtn(e) {
+  e.preventDefault();
+
   for (let i = 0; i < devicesAnimation.length; i++) {
     devicesAnimation[i].style.display = "none";
+
     setTimeout(function () {
       devicesAnimation[i].style.display = "block";
     });
